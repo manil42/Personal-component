cb9f57b6040d1521cd46375c1ce4c528a1626e71
manilsoanitech <manil.maharjan@soanitech.com>
2023-01-31 10:14:06 +0545

    create a buttun.scss file

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,10 @@
-import React, { useEffect, useState } from "react";
-// import logo from "./logo.svg";
-import "./App.css";
-// import './assets/styles/index.scss'
-/* The following line can be included in a src/App.scss */
-
-// import "bootstrap/dist/css/bootstrap.min.css";
-/* The following line can be included in your src/index.js or App.js file */
-
-// import './App.scss';
-import { Dropdown } from "./components/dropdown";
-import { OptionTypes } from "./components/dropdown";
-import "material-symbols";
+import React from "react";
+import Button from "./components/button/Button";
 
 function App() {
-  const [options, setOptions] = useState<OptionTypes[]>();
-
-  const updateValue = (e: any) => {
-    console.log("e", e);
-  };
-
-  useEffect(() => {
-    setOptions([
-      {
-        id: 1,
-        name: "Profile",
-        value: "Profile",
-        avatar: "person",
-      },
-      {
-        id: 1,
-        name: "Settings",
-        value: "Settings",
-        avatar: "settings",
-      },
-      {
-        id: 1,
-        name: "Account",
-        value: "Account",
-        avatar: "lock",
-      },
-    ]);
-  }, []);
   return (
     <div className="App">
-      {options && <Dropdown option={options} handleChange={updateValue} />}
+      <Button />
     </div>
   );
 }
