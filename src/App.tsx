bfb1adf354b97abcb5dfe4f34e50de2a105bbf34
manilsoanitech <manil.maharjan@soanitech.com>
2023-01-31 09:52:31 +0545

    change in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+// import './assets/styles/index.scss'
+/* The following line can be included in a src/App.scss */
+
+// import "bootstrap/dist/css/bootstrap.min.css";
+/* The following line can be included in your src/index.js or App.js file */
+
+// import './App.scss';
+import { Dropdown } from "./components/dropdown";
+import { OptionTypes } from "./components/dropdown";
+import "material-symbols";
 
 function App() {
+  const [options, setOptions] = useState<OptionTypes[]>();
+
+  const updateValue = (e: any) => {
+    console.log("e", e);
+  };
+
+  useEffect(() => {
+    setOptions([
+      {
+        id: 1,
+        name: "Profile",
+        value: "Profile",
+        avatar: "person",
+      },
+      {
+        id: 1,
+        name: "Settings",
+        value: "Settings",
+        avatar: "settings",
+      },
+      {
+        id: 1,
+        name: "Account",
+        value: "Account",
+        avatar: "lock",
+      },
+    ]);
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {options && <Dropdown option={options} handleChange={updateValue} />}
     </div>
   );
 }
