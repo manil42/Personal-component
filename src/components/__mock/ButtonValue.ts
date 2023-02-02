7d88a3adceea2413cde4e1b5037520dd1e4d12a1
joelsoanitech <joel.maharjan@soanitech.com>
2023-02-02 11:48:03 +0545

    Button import fix

---
+++ src/components/__mock/ButtonValue.ts
@@ -0,0 +1,18 @@
+import { ButtonProps } from "../button";
+
+export const ButtonLabel: ButtonProps = {
+  label: "button",
+  varient: "contained",
+  isdisabled: true,
+};
+export const ButtonIcon: ButtonProps = {
+  icon: "person",
+  varient: "contained",
+  size: "small",
+};
+
+export const ButtonLabelIcon: ButtonProps = {
+  label: "Person",
+  icon: "person",
+  varient: "contained",
+};
