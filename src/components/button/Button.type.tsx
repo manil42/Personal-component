36f6b426f7e3b5f9556cb3bdc1cfb01a7d0b0894
manilsoanitech <manil.maharjan@soanitech.com>
2023-02-07 16:53:00 +0545

    commit size

--- src/components/button/Button.type.tsx
+++ src/components/button/Button.type.tsx
@@ -5,5 +5,5 @@ export interface ButtonProps {
   varient?: "text" | "contained" | "outlined";
   theme?: "primary" | "secondary" | "sucess" | "danger";
   shape?: "circle" | "square" | "rounded";
-  size?: "small" | "medium" | "large";
+  // size?: "small" | "medium" | "large";
 }
