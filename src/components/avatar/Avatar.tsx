4ee3f1bad1c5996e05bfaa847e4281f89a8381d1
joelsoanitech <joel.maharjan@soanitech.com>
2023-02-20 12:00:46 +0545

    varient added in avatar

--- src/components/avatar/Avatar.tsx
+++ src/components/avatar/Avatar.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 import { AvatarProps } from "./Avatar.type";
 
-const Avatar = ({ name, imageUrl }: AvatarProps) => {
+const Avatar = ({ name, imageUrl, size, varient, letter }: AvatarProps) => {
   return (
-    <div className="avatar">
-      {imageUrl && <img className="avatar-image" src={imageUrl} alt="avatar" />}
-      {imageUrl === undefined && name !== "" && (
-        <div className="avatar-text">{name?.substring(0, 2)}</div>
+    <div className={`avatar ${size ? `avatar-${size}` : ""}`}>
+      {imageUrl && (
+        <img
+          className={`avatar-image ${varient ? `avatar-${varient}` : ""}`}
+          src={imageUrl}
+          alt="avatar"
+        />
       )}
+      {imageUrl === undefined && name !== "" && (
+        <div className={`avatar-text avatar-${varient}`}>
+          {name?.split(" ")[0][0]}
+          {name?.split(" ")[1][0]}
+        </div>
+      )}
+
     </div>
   );
 };
--- src/components/avatar/Avatar.type.tsx
+++ src/components/avatar/Avatar.type.tsx
@@ -1,4 +1,7 @@
-export interface AvatarProps{
-    imageUrl?: any;
-    name?: string;
+export interface AvatarProps {
+  imageUrl?: any;
+  name?: string;
+  size?: "small" | "medium" | "large";
+  varient?: "square" | "rounded";
+  letter?: string;
 }
