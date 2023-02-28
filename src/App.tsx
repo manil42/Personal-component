465b43f63b014a5793339abbeaf1e6c9a0f2445d
manilsoanitech <manil.maharjan@soanitech.com>
2023-02-28 15:06:50 +0545

    making app.tsx file empty

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,7 @@
-import Tooltip from "components/tooltip/Tooltip";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./App.css";
-import Button from "components/button/Button";
 function App() {
-  return (
-    <div style={{ position: "relative" }}>
-      <div style={{ position: "absolute", top: "100px", left: "150px" }}>
-        <Tooltip description="header2" direction="bottom">
-          <Button label="button button" varient="contained" />
-        </Tooltip>
-      </div>
-      <Button label="button button button" varient="contained" />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
