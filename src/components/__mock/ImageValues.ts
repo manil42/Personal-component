a7088f9c4de72302fcf17d23b43ef4138e16c0c1
joelsoanitech <joel.maharjan@soanitech.com>
2023-03-02 10:13:30 +0545

    image height width 100%

--- src/components/__mock/ImageValues.ts
+++ src/components/__mock/ImageValues.ts
@@ -4,7 +4,6 @@ import { ImageProps } from "components/image/Image.type";
 export const DefaultImageValues: ImageProps = {
     src: "https://upload.wikimedia.org/wikipedia/commons/4/49/Elon_Musk_2015.jpg",
     alt: "elon musk",
-    height:"100px",
-    width:"100px",
+
     loading:"lazy",
 }
--- src/components/image/Image.type.tsx
+++ src/components/image/Image.type.tsx
@@ -1,8 +1,8 @@
 export interface ImageProps {
   src: string;
   alt: string;
-  width: string;
-  height: string;
+  width?: string;
+  height?: string;
   loading: "lazy" | "eager";
   variant?: "rounded" | "circle" | "rectangle";
   onLoad?: () => void;
